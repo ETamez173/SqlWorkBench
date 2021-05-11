3268925731b0282ebcf5ed3d3d7e99110fb56e27
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-05-11 14:21:57 -0500

    practice NULL, CONCAT

---
+++ movies_NULL_CONCAT_ALIAS.sql
@@ -0,0 +1,121 @@
+
+
+-- Dealing with NULL values
+
+/*
+SELECT FROM tablename
+WHERE columnname IS NULL;
+
+SELECT FROM tablename
+WHERE columname IS NOT NULL;
+
+
+*/
+
+SELECT * FROM actors;
+
+SELECT * FROM actors
+WHERE date_of_birth IS NULL;
+
+SELECT * FROM actors
+WHERE date_of_birth IS NOT NULL;
+
+SELECT * FROM movie_revenues
+WHERE domestic_takings IS NOT NULL
+ORDER BY domestic_takings DESC;
+
+
+SELECT * FROM movie_revenues
+WHERE international_takings IS NULL;
+
+
+-- Setting a columnname AS new_columnname FROM tablename;
+
+
+SELECT last_name FROM directors;
+
+SELECT last_name AS surname FROM directors;
+
+SELECT * FROM directors;
+
+SELECT last_name AS surname FROM directors
+WHERE last_name = 'Anderson';
+
+
+SELECT last_name AS surname FROM directors
+WHERE last_name LIKE 'A%'
+--ORDER BY surname;
+ORDER BY last_name;
+
+
+-- Using concatenate
+
+/*
+
+SELECT 'string1' || 'string2' AS new_string;
+
+SELECT CONCAT(column1, column2) AS new_column FROM tablename;
+
+SELECT CONCAT_WS(' ',column1, column2) AS new_column FROM tablename;
+
+*/
+
+SELECT 'concat' || 'together' AS new_string;
+
+SELECT 'concat' || ' ' || 'together' AS new_string;
+
+
+SELECT CONCAT(first_name, last_name) AS full_name FROM actors;
+
+SELECT CONCAT(first_name,' ', last_name) AS full_name FROM actors;
+
+SELECT CONCAT_WS(' ', first_name, last_name, date_of_birth) AS full_name FROM actors;
+
+
+--Challenge
+
+-- #1 Return the top 3 movies with highest international takings
+
+SELECT * FROM movie_revenues
+WHERE international_takings IS NOT NULL
+ORDER BY international_takings DESC
+LIMIT 3;
+
+-- #2 Concatenate the first and last names of the directors, seperated by a space,
+-- and call this new column full_name
+
+SELECT CONCAT_WS(' ', first_name, last_name) AS full_name FROM directors;
+
+-- #3 Return the actors with missing first_names or missing date_of_births
+
+SELECT * FROM actors
+WHERE first_name IS NULL
+OR date_of_birth IS NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
